f9cf584f244a4d6e94138f303f63a402664f4a67
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-01-19 01:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (87.12%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -14,14 +14,14 @@ public:
         if(!head || !head->next){
             return head;
         }
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenHead=even;
         while(even && even->next){
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = odd->next;
-            even = even->next;
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
         }
         odd->next=evenHead;
         return head;
