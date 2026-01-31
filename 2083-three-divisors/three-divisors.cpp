d324e95fd5e1eb763bdcea2c55ffaf6a5a27c819
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-02-01 00:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (22.10%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int root = sqrt(n);
+        if(root * root != n) return false; 
+
+        if(root < 2) return false;
+        for(int i = 2; i * i <= root; i++) {
+            if(root % i == 0) return false;
+        }
+        return true;
+    }
+};
