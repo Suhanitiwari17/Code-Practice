0678c532ed9e482521e15b576646d4ba7bbb8fd6
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-02-15 11:12:25 +0530

    Time: 1 ms (72.80%) | Memory: 12.7 MB (74.63%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<long long> st;
+        long long res = 0, num = 0, sign = 1;
+
+        for(int i=0; i<s.size();i++){
+            if(isdigit(s[i])){
+                num = num *10 + (s[i]-'0');
+            }
+
+            else if(s[i] == '+' || s[i] == '-'){
+                res += sign*num;
+                num = 0;
+                sign = (s[i] == '+')?1:-1;
+            }
+
+            else if(s[i] == '('){
+                st.push(res);
+                st.push(sign);
+                res = 0;
+                sign = 1;
+            }
+
+            else if(s[i] == ')'){
+                res += sign*num;
+                num = 0;
+                res *= st.top();
+                st.pop();
+                res += st.top();
+                st.pop();
+            }
+        }
+        return (int)res+sign*num;
+    }
+};
