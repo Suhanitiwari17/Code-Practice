ec3981ae3780323f9efcc88fd1159a62b3b9f6c6
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-06-12 23:19:20 +0530

    Time: 5 ms (64.47%) | Memory: 21.1 MB (49.62%) - LeetSync

---
+++ 2447-merge-similar-items/merge-similar-items.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1,
+                                          vector<vector<int>>& items2) {
+
+        map<int,int> mp;
+
+        for(auto &item : items1){
+            mp[item[0]] += item[1];
+        }
+
+        for(auto &item : items2){
+            mp[item[0]] += item[1];
+        }
+
+        vector<vector<int>> ans;
+
+        for(auto &it : mp){
+            ans.push_back({it.first, it.second});
+        }
+
+        return ans;
+    }
+};
