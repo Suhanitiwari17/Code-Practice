5e4760c48b1288ddd95f7db71a43602ccd02a3dd
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-08-07 00:24:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (73.40%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+       
+
+        while(true){
+           int num = n;
+
+           int pro = 1;
+
+           while(num > 0){
+              int rem = num % 10;
+              pro = pro * rem;
+              num = num/10;
+           }
+
+           if(pro % t == 0) return n;
+
+           n++;
+
+
+        }
+    }
+};
