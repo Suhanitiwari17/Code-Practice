fc84d43f743da232ea2a2b532dfbdf7a6d999c6b
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-08-22 17:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    //nt count;
     int t[46];
-    int solve(int n,int idx){
-        
+    int solve(int n){
         if(n<0) return 0;
 
-        if(n==0) return 1;
+        if(t[n] != -1) return t[n];
 
-        if(t[idx] != -1) return t[idx];
+        if(n == 0) return 1;
 
-        int one_step = solve(n-1,idx+1);
+        int one_step = solve(n-1);
+        int two_step = solve(n-2);
 
-        int two_step = solve(n-2,idx+2);
-
-        return t[idx] = one_step + two_step;
+        return t[n] = one_step + two_step;
     }
     int climbStairs(int n) {
         memset(t,-1,sizeof(t));
-
-        return solve(n,0);
+        return solve(n);
     }
 };
