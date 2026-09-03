55216398460e51cb4bb00f8d6d8271bf72ecc4a1
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-09-03 16:04:37 +0530

    Time: 211 ms (16.92%) | Memory: 31 MB (23.23%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int t[1001][1001];
-    int solve(vector<vector<int>>& pairs , int i, int prev){
-        if(i >= pairs.size()) return 0;
+    int solve(vector<vector<int>>& nums , int prev , int curr){
+        int n=nums.size();
+
+        if(curr == n) return 0;
+
+        if(prev != -1 &&  t[prev][curr] != -1) return t[prev][curr];
 
         int take = 0;
-
-        if(prev != -1 && t[i][prev] != -1) return t[i][prev];
-        
-        if(prev == -1 || pairs[i][0] > pairs[prev][1]){
-            take = 1 + solve(pairs , i + 1 , i);
+        if(prev == -1 || nums[curr][0] > nums[prev][1]){
+            take = 1 + solve(nums , curr , curr+1);
         }
 
-        int skip = solve(pairs , i+1 , prev);
+        int skip = solve(nums , prev , curr+1);
 
-        if(prev != -1) t[i][prev] = max(take,skip);
+        if(prev != -1) t[prev][curr] = max(take,skip);
 
         return max(take,skip);
-
     }
     int findLongestChain(vector<vector<int>>& pairs) {
         memset(t,-1,sizeof(t));
+        sort(pairs.begin(),pairs.end());
 
-        sort(begin(pairs) , end(pairs));
-
-        return solve(pairs,0,-1);
+        return solve(pairs,-1,0);
     }
 };
