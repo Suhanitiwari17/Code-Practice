7aa81fe4b60166d9ba473896c1eb461d67899f69
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2026-09-07 20:45:15 +0530

    Time: 493 ms (5.12%) | Memory: 7.9 MB (96.74%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    bool solve(string &s, string &p, int i, int j){
-        if(j == p.length()){
+    bool solve(int i , int j, string& s , string& p){
+        if(j == p.length()) {
             return i == s.length();
         }
+
         bool fcm = false;
 
-        if(i < s.length() && (s[i] == p[j] || p[j] == '.')){
-            fcm = true;
-        }
+        if(i < s.length() && ( s[i] == p[j] || p[j]=='.' ) ) fcm = true;
 
         if(p[j+1] == '*'){
-            bool not_take = solve(s, p, i, j+2);
-            bool take  = fcm && solve(s, p, i+1, j);
-            return take || not_take;
+            bool skip = solve(i , j+2 , s , p);
+            bool take = fcm && solve(i+1 , j , s , p);
+
+            return take || skip;
         }
         else{
-            return fcm && solve(s, p, i+1, j+1);
+            return fcm && solve(i+1 , j+1 , s , p);
         }
     }
     bool isMatch(string s, string p) {
-        if(s.length() == 0){
-            return true;
-        }
-
-        bool check = solve(s, p, 0, 0);
-        return check;
+        return solve(0,0,s,p);
     }
 };
